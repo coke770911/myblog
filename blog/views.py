f0b09511ed09f6db833600655c4e4e3169c7d944
ChenJoey <coke770911@gmail.com>
2017-07-26 20:32:20 +0800

    產生後台

---
+++ blog/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import redirect, render
+from django.utils import timezone
+
+from .models import Post
+
+# Create your views here.
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
+
+
+def post_create(request):
+    if request.method == "POST":
+        new_title = request.POST.get('title')
+        new_text = request.POST.get('text')
+        new_post = Post(title=new_title, text=new_text)
+
+        new_post.save()
+
+        new_post.publish()
+        return redirect('post_list')
+
+    return render(request, 'blog/post_create.html', {})
